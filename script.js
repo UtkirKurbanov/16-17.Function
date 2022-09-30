8313c67925e7e2cd2df4c4a18a12a815b9f72c48
Utkir <utkirkurbanov2700@gmail.com>
2022-09-30 17:28:15 +0500

    First commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var sayHelloTo = function (userName) {
+  console.log("Hello, " + userName);
+};
+
+sayHelloTo("John Doe");
+
+// function sayHelloTo(name) {
+//   console.log("Hello, " + name);
+// }
+
+// sayHelloTo("Nick Brown");
